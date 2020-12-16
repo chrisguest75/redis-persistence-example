5bf8fb301a8b97141cf2c2ba86cb667f2d4ba790
Chris Guest <chris.guest@condenastint.com>
2020-12-16 22:15:19 +0000

    Timing of word loading

--- pyworker/main.py
+++ pyworker/main.py
@@ -2,9 +2,10 @@ import redis
 import os
 import io
 import time 
+from code_timer import CodeTimer
+import argparse
 
 # TODO:
-# Timing outputs
 # Load as a list
 # Load with less requests
 # Pickle?
@@ -14,9 +15,12 @@ def load_words_into_redis(redis, words):
     '''
     Load words into redis as keys - slow.......
     '''
+    ct = CodeTimer()    
+
     print("Loading words")
     count = 0
     first_word = ""
+    ct.start()
     for word in words:
         redis.mset({word:"true"})
         count += 1
@@ -24,32 +28,43 @@ def load_words_into_redis(redis, words):
             first_word = word
 
         if count % 1000 == 0:
-            print(str(count) + " words loaded - first - '" + first_word + "' last - '" + word + "'")
+            print(f"{count} words loaded - first:'{first_word}' last:'{word}' '{ct.stop():0.4f}' secs")
             first_word = ""
+            ct.start()
 
+    print(f"{count} words loaded - first:'{first_word}' last:'{word}' '{ct.stop():0.4f}' secs")
     print("Finished")
 
 
 if __name__ == '__main__':
-    server_url = "0.0.0.0"    
-    server_port = int("6379")    
+    parser = argparse.ArgumentParser(description='Redis dataload example')
+    parser.add_argument('--method', dest='method', type=str, help='Name of method for data loading')
+    parser.add_argument('--server', default="0.0.0.0", dest='server', type=str, help='')
+    parser.add_argument('--port', default="6379", dest='port', type=str, help='')
+
+    args = parser.parse_args()
+
+    server_url = args.server   
+    server_port = int(args.port)    
     if 'SERVER_URL' in os.environ:
         server_url = os.environ['SERVER_URL']
     if 'SERVER_PORT' in os.environ:
         server_port = int(os.environ['SERVER_PORT'])
 
+    print(f"{server_url}:{server_port}")
     redis = redis.Redis(host=server_url, port=server_port)
 
     # load the word list
-    start = time.perf_counter()
+    ct = CodeTimer()    
+    ct.start()
     filepath = "./word-list.txt"
     print(f"Load file '{filepath}'")
     with(io.open(filepath)) as f:
-        words = [line.rstrip() for line in f]
-    print(f"Time taken to load file '{time.perf_counter() - start:0.4f}' secs")
+        words = [line.rstrip() for line in f]   
+    print(f"Time taken to load file '{ct.stop():0.4f}' secs")
 
     # push into redis
-    start = time.perf_counter()
+    ct.start()
     load_words_into_redis(redis, words)
-    print(f"Time taken to load words into Redis '{time.perf_counter() - start:0.4f}' secs")
+    print(f"Time taken to load words into Redis '{ct.stop():0.4f}' secs")
 
